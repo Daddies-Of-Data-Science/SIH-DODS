c0ec087fadbdcd8f740c4581708884be81a2e49a
Aaditya Malani <aadityamalanii15@gmail.com>
2024-09-09 12:36:28 +0530

    routing updated and navbar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './Pages/Home/Home';
 // import IprPage from './pages/IprPage';
-// import ResearchPage from './pages/ResearchPage';
-// import StartupPage from './pages/StartupPage';
-// import InvestorPage from './pages/InvestorPage';
+import ResearchPage from './Pages/Research/Research'
+import { NavbarDefault } from './components/Navbar';
+import StartupPage from './Pages/Startup/Startup';
+import InvestorPage from './Pages/Investor/Investor';
 
 function App() {
   return (
     <Router>
+      <NavbarDefault/>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/ipr" element={<IprPage />} />
+         {/* <Route path="/ipr" element={<IprPage />} /> */}
         <Route path="/research" element={<ResearchPage />} />
         <Route path="/startup" element={<StartupPage />} />
-        <Route path="/investor" element={<InvestorPage />} /> */}
+        <Route path="/investor" element={<InvestorPage />} /> 
       </Routes>
     </Router>
   );
