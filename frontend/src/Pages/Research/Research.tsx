ee48a263722e5051988724f42a585c185d933017
Aaditya Malani <aadityamalanii15@gmail.com>
2024-09-10 00:52:37 +0530

    build error fixed

--- frontend/src/Pages/Research/Research.tsx
+++ frontend/src/Pages/Research/Research.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import HomePage from "../../components/research/HomePage";
 import ProjectList from "../../components/research/ProjectList";
 import ResearchPapers from "../../components/research/ResearchPaper";
