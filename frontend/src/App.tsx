1ad4e1e3ba165e5abd573c6d627b84f1d5dcc32c
Aaron Sequeira <sequeiraaaron100@gmail.com>
2024-09-12 13:18:11 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import IprPage from './pages/IprPage';
 import ResearchPage from '../../frontend/src/pages/Research/Research'
 import { NavbarDefault } from './components/Navbar';
-import StartupPage from '../../frontend/src/Pages/Startup/Startup';
-import InvestorPage from '../../frontend/src/Pages/Investor/Investor';
-import HomePage from '../../frontend/src/Pages/Home/Home';
-import Dashboard from './Pages/Research/ResearchDashboard';
-import StartupDashboard from './Pages/Startup/Dashboard/Dashboard';
+import StartupPage from '../../frontend/src/pages/Startup/Startup';
+import InvestorPage from '../../frontend/src/pages/Investor/Investor';
+import HomePage from '../../frontend/src/pages/Home/Home';
+import Dashboard from './pages/Research/ResearchDashboard';
+import StartupDashboard from './pages/Startup/Dashboard/Dashboard';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import InvestorDashboard from './Pages/Investor/InvestorDashboard';
+import InvestorDashboard from './pages/Investor/InvestorDashboard';
 
 function App() {
   return (
