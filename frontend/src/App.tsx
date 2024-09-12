dc8894976eb1d5580670d9b639a34cec636019f6
DJSCE Student <DJSCE.Student@SVKMGRP.COM>
2024-09-12 12:36:29 +0530

    build error fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import IprPage from './pages/IprPage';
-import ResearchPage from '../../frontend/src/pages/Research/Research'
+import ResearchPage from '../../frontend/src/Pages/Research/Research'
 import { NavbarDefault } from './components/Navbar';
-import StartupPage from '../../frontend/src/pages/Startup/Startup';
-import InvestorPage from '../../frontend/src/pages/Investor/Investor';
-import HomePage from '../../frontend/src/pages/Home/Home';
-import Dashboard from './pages/Research/ResearchDashboard';
-import StartupDashboard from './pages/Startup/Dashboard/Dashboard';
-import InvestorDasboard from './pages/Investor/InvestorDasboard';
+import StartupPage from '../../frontend/src/Pages/Startup/Startup';
+import InvestorPage from '../../frontend/src/Pages/Investor/Investor';
+import HomePage from '../../frontend/src/Pages/Home/Home';
+import Dashboard from './Pages/Research/ResearchDashboard';
+import StartupDashboard from './Pages/Startup/Dashboard/Dashboard';
+import InvestorDasboard from './Pages/Investor/InvestorDasboard';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
